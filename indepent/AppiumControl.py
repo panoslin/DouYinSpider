bc40c5ad4f464113b5642c0d8ebccc4bef6f5a1c
panos <35588706+panoslin@users.noreply.github.com>
2018-09-12 15:59:43 +0800

    Update AppiumControl.py
    Fix the problem of swiping out of control

--- indepent/AppiumControl.py
+++ indepent/AppiumControl.py
@@ -11,6 +11,10 @@ from random import randint
 
 
 def swipe():
+    """
+    swipe on the emulator
+    :return:
+    """
     time.sleep(2)
     TouchAction(driver).press(x=randint(200, 500), y=940).move_to(x=randint(200, 500), y=200).release().perform()
 
@@ -19,30 +23,32 @@ if __name__ == '__main__':
     # This sample code uses the Appium python client
     # pip install Appium-Python-Client
     # Then you can paste this into a file and simply run with Python
-
-    caps = {}
-    caps["platformName"] = "Android"
-    caps["deviceName"] = "SAMSUNG_SM_N900A"
-    caps["appPackage"] = "com.ss.android.ugc.aweme"
-    caps["appActivity"] = ".main.MainActivity"
-
-    driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
-    swipe()
     while True:
-        next = choice(
-            ['fit', 'Notfit', 'Notfit', 'Notfit', 'Notfit', 'Notfit'])  # one fit in one request(i.e. six videos)
-        if next == 'fit':
-            time.sleep(randint(5, 40))
-            TouchAction(driver).tap(x=658, y=452).perform()
-            swipe()
-            swipe()
-            # time.sleep(2)
-            # TouchAction(driver).tap(x=357, y=935).perform()
-            # time.sleep(randint(5, 40))
-            # TouchAction(driver).tap(x=50, y=109).perform()
-            time.sleep(2)
-            TouchAction(driver).tap(x=50, y=109).perform()
-            swipe()
+        StartTime = int(time.time())
+        #The necessary parms to convert to appium server
+        caps = {}
+        caps["platformName"] = "Android"
+        caps["deviceName"] = "SAMSUNG_SM_N900A"
+        # Next two ele can be found by bash command:
+        # adb logcat ActivityManager:I*:s
+        caps["appPackage"] = "com.ss.android.ugc.aweme"#DouYin package adress
+        caps["appActivity"] = ".main.MainActivity"#DouYin lauching activity
+        # Remote driver connection to appium server
+        driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+        swipe()
+        # Start to loop the action chain
+        while (int(time.time()) - StartTime) % 1800 != 0:# Restart every 30mins
+            next = choice(
+                ['fit', 'Notfit', 'Notfit', 'Notfit', 'Notfit', 'Notfit', 'Notfit', 'Notfit', 'Notfit', 'Notfit',
+                 'Notfit', 'Notfit'])  # one fit in 2 request(i.e. 12 videos)
+            if next == 'fit':
+                time.sleep(randint(10, 40))
+                TouchAction(driver).tap(x=658, y=452).perform()
+                swipe()
+                swipe()
+                time.sleep(2)
+                TouchAction(driver).tap(x=50, y=109).perform()
+                swipe()
 
-        elif next == 'Notfit':
-            swipe()
+            elif next == 'Notfit':
+                swipe()
