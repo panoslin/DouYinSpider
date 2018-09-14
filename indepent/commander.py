66443719d1690c00c46a5d9cd26fa04bb21e859e
panos <35588706+panoslin@users.noreply.github.com>
2018-09-14 10:39:57 +0800

    Delete commander.py

--- indepent/commander.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Created by panos on 2018/9/7
-# IDE: PyCharm
-import subprocess
-
-if __name__ == '__main__':
-    subprocess.call(['mitmdump', '-s',' .\FlowCatcher.py'])
