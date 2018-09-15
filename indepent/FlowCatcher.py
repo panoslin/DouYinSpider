a88f02da838c64d2a0f1eb986d5e09beb06b7748
panos <35588706+panoslin@users.noreply.github.com>
2018-09-15 19:29:22 +0800

    Update FlowCatcher.py
    Filter the videos that had already been downloaded

--- indepent/FlowCatcher.py
+++ indepent/FlowCatcher.py
@@ -26,33 +26,35 @@ def response(flow):
         data = json.loads(text)['aweme_list']
         for aweme in data:
             if aweme['is_ads']: # identify whether if the current video is commercial
-                res = dict()
-                res['is_ads'] = aweme['is_ads']
-                res['avatar_url'] = aweme['author']['avatar_larger']['url_list'][0]
-                res['register_time'] = aweme['author']['create_time']
-                res['authorname'] = aweme['author']['nickname']
-                res['author_user_id'] = aweme['author_user_id']
-                res['aweme_id'] = aweme['aweme_id']
-                res['broadcast_time'] = aweme['create_time']
-                res['desc'] = aweme['desc']
-                res['duration'] = int(str(aweme['duration'])[:-3])
-                res['comment_count'] = aweme['statistics']['comment_count']
-                res['digg_count'] = aweme['statistics']['digg_count']
-                res['forward_count'] = aweme['statistics']['forward_count']
-                res['play_count'] = aweme['statistics']['play_count']
-                res['share_count'] = aweme['statistics']['share_count']
-                res['video_url'] = aweme['video']['play_addr']['url_list'][0]
-                res['height'] = aweme['video']['height']
-                res['width'] = aweme['video']['width']
-                res['uid'] = str(uuid.uuid1())
-                # Download the video
-                with open('Download/{video_name}.mp4'.format(video_name=res['uid']), 'wb') as video:
-                    video.write(requests.get(res['video_url']).content)
-                while True:# Get the size of the video when it is downloaded completely
-                    try:
-                        res['size'] = os.path.getsize('Download/{video_name}.mp4'.format(video_name=res['uid']))
-                        break
-                    except Exception:
-                        continue
-                # Insert the data into redis database
-                r.hset('ame:data:Ver1', res['aweme_id'], res)
+                # determine whether if the video exists
+                if not r.hexists("ame:data:Ver1", aweme['aweme_id']):
+                    res = dict()
+                    res['is_ads'] = aweme['is_ads']
+                    res['avatar_url'] = aweme['author']['avatar_larger']['url_list'][0]
+                    res['register_time'] = aweme['author']['create_time']
+                    res['authorname'] = aweme['author']['nickname']
+                    res['author_user_id'] = aweme['author_user_id']
+                    res['aweme_id'] = aweme['aweme_id']
+                    res['broadcast_time'] = aweme['create_time']
+                    res['desc'] = aweme['desc']
+                    res['duration'] = int(str(aweme['duration'])[:-3])
+                    res['comment_count'] = aweme['statistics']['comment_count']
+                    res['digg_count'] = aweme['statistics']['digg_count']
+                    res['forward_count'] = aweme['statistics']['forward_count']
+                    res['play_count'] = aweme['statistics']['play_count']
+                    res['share_count'] = aweme['statistics']['share_count']
+                    res['video_url'] = aweme['video']['play_addr']['url_list'][0]
+                    res['height'] = aweme['video']['height']
+                    res['width'] = aweme['video']['width']
+                    res['uid'] = str(uuid.uuid1())
+                    # Download the video
+                    with open('Download/{video_name}.mp4'.format(video_name=res['uid']), 'wb') as video:
+                        video.write(requests.get(res['video_url']).content)
+                    while True:# Get the size of the video when it is downloaded completely
+                        try:
+                            res['size'] = os.path.getsize('Download/{video_name}.mp4'.format(video_name=res['uid']))
+                            break
+                        except Exception:
+                            continue
+                    # Insert the data into redis database
+                    r.hset('ame:data:Ver1', res['aweme_id'], res)
